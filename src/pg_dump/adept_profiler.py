85db80518261c4a3deaba13d9e1f0dddd59e05b3
James Kimani <james@eoncollective.com>
2023-05-09 22:45:34 -0400

    added adept_profiler

---
+++ src/pg_dump/adept_profiler.py
@@ -0,0 +1,80 @@
+import os
+import argparse
+import uuid
+import re
+import pandas
+from ydata_profiling import ProfileReport
+from pathlib import Path
+
+def profile_adept_from_folder(args):
+    """
+    profile_adept_results from a folder
+    """
+    try:
+        stats_output_path = args.output_dir
+        ddl_files = Path(directory).glob('*.csv')
+        dfs = list()
+        for f in ddl_files:
+            # print("Checking out "+ str(f))
+            data = pandas.read_csv(f,
+                                sep = ';', 
+                                engine='python')
+            # data = pandas.read_fwf(f)
+            # .stem is method for pathlib objects to get the filename w/o the extension
+            data['file'] = f.stem
+            dfs.append(data)
+
+        print (stats_output_path)
+        df = pandas.concat(dfs, ignore_index=True)
+        prof = ProfileReport(df,
+            config_file=args.conf_file)
+        prof.to_file(output_file=args.output_dir+'/report.html')
+    except:
+        print ("something went wrong in profile_adept_results")
+        raise
+
+def profile_adept_from_file(args):
+    """
+    profile_adept_results from a single file
+
+    """
+    try:
+        stats_output_path = args.output_dir
+        print (stats_output_path)
+        df = pandas.read_csv(stats_output_path,
+                                sep = ';',
+                                engine='python')
+        prof = ProfileReport(df,
+            config_file=args.conf_file)
+        prof.to_file(output_file=args.output_dir+'/report.html')
+    except:
+        print ("something went wrong in profile_adept_results")
+        raise
+
+def main(parser):
+    """
+    main - Driver program
+    
+    """
+    try:
+        args = parser.parse_args()
+        if args.mode.upper() == "FILE":
+            profile_adept_from_file(args)
+        elif args.mode.upper() == "FOLDER":
+            profile_adept_from_folder(args)
+    except:
+        print ("something went wrong in calling main driver for the program")
+        raise
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+    prog='adept_profiler',
+    description='''This program takes in a pg_dump generated DDLs and generates individual 
+            .sql files partitioned by object type: TABLES, VIEWS etc.''',
+    epilog='ADEPT utilities')
+    parser.add_argument('--version', action='version', version='%(prog)s 1.0')
+    parser.add_argument("-i", "--input-file", required=False, help="input txt", default="/Users/james.kimani/Development/repositories/greenplum-oss-docker/usecase2/data/gp_ns_ddl_test-schema-eon-assessment.sql")
+    parser.add_argument("-o", "--output-dir", required=False, help="output directory", default="/Users/james.kimani/Development/repositories/greenplum-oss-docker/usecase2/data/splits/")
+    parser.add_argument("-c", "--conf-file", required=False, help="Profiling configuration file", default="profiling.yml")
+    parser.add_argument("-m", "--mode", required=False, help="choose mode to run this jobs as: can be file or folder", choices=['file','folder', 'FILE', 'FOLDER'], default="FILE")
+    main(parser)
